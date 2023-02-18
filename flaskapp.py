045c99a9f4c8bd5ae5618a3bad6bad736b2aa7d5
Malik <inghammalik@icloud.com>
2023-02-17 22:16:06 -0500

    More bug fixes

--- flaskapp.py
+++ flaskapp.py
@@ -29,28 +29,22 @@ def search():
         f'{oath_secret}'
     )
 
-    #General blog information. Here I'm going to get the max posts from the specified blog
     blog_name = request.form.get('blog_name')
-    # return render_template('search.html', blog_name=blog_name)
-    # print('blog name: ', blog_name)
 
     blog = client.blog_info(blog_name)
-    # print('blog: ', blog)
+    
     max_posts = blog['blog']['posts']
     
-    list_of_posts = []
+    original_posts = []
 
     offset = 0
     count = 0
-    # print(max_posts)
     while max_posts > offset:
         count += 1
-        # print(count)
         if count % 10 == 0:
             print('sleeping dont bother me')
             time.sleep(60)
 
-        # print('Current offset: ',offset)
         posts = client.posts(blog_name, reblog_info=False, notes_info=False, \
             limit=10, offset=offset)
         try:
@@ -58,7 +52,7 @@ def search():
                 try:
                     trail = item.get('trail')[0]
                     if trail['blog'].get('name') == blog_name:
-                        list_of_posts.append(item)
+                        original_posts.append(item)
                 except TypeError:
                     pass
         
@@ -73,20 +67,7 @@ def search():
         if offset == max_posts:
             break
 
-        # response = {
-        #     'photo': posts['image'],
-        #     'notes': posts['notes'],
-        #     'url': posts['url']
-        # }
-    # print('list of posts: ', list_of_posts)
-    print(list_of_posts[0])
-
-
-    # print(list_of_posts)
-    # print('outside for loop')
-    # for post in list_of_posts:
-    #     print(post['post_url'])
-    # print(list_of_posts)
+    print(original_posts[0])
 
 @app.route('/results')
 def results(list_of_posts=None):
@@ -96,30 +77,6 @@ def results(list_of_posts=None):
         return render_template('posts.html')
     else:
         return render_template('error.html')
-
-
-
-
-#not used at the moment. I'll circle back to this asap 
-def get_all_posts(max_posts=None, current_offset=None, blog_name=None):
-    '''Recursive function call to iterate through all posts '''
-
-    client = TumblrRestClient(
-        f'{consumer_key}',
-        f'{consumer_secret_key}',
-        f'{oath_token}',
-        f'{oath_secret}'
-    )
-    
-    client.posts('sazzmine', reblog_info=False, notes_info=False, limit=50, offset=current_offset)
-
-    if current_offset >= max_posts:
-        return False
-
-    else:
-        if current_offset + 50 > max_posts:
-            return get_all_posts(max_posts, current_offset + (max_posts-current_offset))
-        return get_all_posts(max_posts, current_offset+50)
 
 
 if __name__ == '__main__':
